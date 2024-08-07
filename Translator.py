75a8a434ec36c07beabe9ad4f7cedfae4d090239
zhannai <zhangxinpei22@mails.ucas.ac.cn>
2024-08-07 16:38:00 +0800

    0807更新，加入暂存的区分点边面功能

--- Translator.py
+++ Translator.py
@@ -1,39 +1,90 @@
 import gmsh
-#C. Geuzaine and J.-F. Remacle. Gmsh: a three-dimensional finite element mesh generator with built-in pre- and post-processing facilities. International Journal for Numerical Methods in Engineering 79(11), pp. 1309-1331, 2009. 
+
+# C. Geuzaine and J.-F. Remacle. Gmsh: a three-dimensional finite element mesh generator with built-in pre- and post-processing facilities. International Journal for Numerical Methods in Engineering 79(11), pp. 1309-1331, 2009.
 import sys
 import os
 
-def Trans_Geometry(file_paths, output_path, output_format):
-    for file_path in file_paths:
-        out_last_name=output_format.split("*")[1][:-1]
-        #print(out_last_name)
-        #print(file_path)
-        #print(os.path.split(file_path))
-        #print(output_format)
-        gmsh.initialize()
-        gmsh.model.add("model")
-        gmsh.open(file_path)
-        out_name=os.path.splitext(os.path.split(file_path)[1])
-        print(os.path.join(output_path,out_name[0])+out_last_name)
-        #pause=input("Press Enter to continue...")
-        gmsh.write(os.path.join(output_path,out_name[0])+out_last_name)
-        gmsh.clear()
-        gmsh.finalize()
 
-def Trans_Mesh(file_paths, output_path, output_format):
-    for file_path in file_paths:
-        out_last_name=output_format.split("*")[1][:-1]
-        #print(out_last_name)
-        #print(file_path)
-        #print(os.path.split(file_path))
-        #print(output_format)
+class MyTranslator:
+    def __init__(self):
+        self.nodeTags = []
+        self.nodeCoords = []
+        self.nodeParams = []
+        self.edgeNodes = []
+        self.edgeTags = []
+        self.faceNodes = []
+        self.faceTags = []
+        self.volumes = []
+        pass
+
+    def Get_Mesh_Dim_Elements(self, file_path):
+        #  本函数实现可以从Gmsh Python extended tutorial 7找指令
         gmsh.initialize()
         gmsh.model.add("model")
         gmsh.open(file_path)
-        out_name=os.path.splitext(os.path.split(file_path)[1])
-        print(os.path.join(output_path,out_name[0])+out_last_name)
-        #pause=input("Press Enter to continue...")
-        gmsh.write(os.path.join(output_path,out_name[0])+out_last_name)
+        # 获取网格中体的个数
+        self.volumes = gmsh.model.getEntities(3)
+        # 获取网格中点的个数
+        for dim, tag in self.volumes:
+            nodeTags, nodeCoords, nodeParams = gmsh.model.mesh.getNodes(dim, tag)
+            self.nodeTags += nodeTags
+            self.nodeCoords += nodeCoords
+            self.nodeParams += nodeParams
+        print(len(self.nodeTags))
+        # 获取网格中边的个数
+        gmsh.model.mesh.createEdges()
+        # edgeNodes是edgeTags的两倍，每个边有两个节点
+        self.edgeTags, self.edgeNodes = gmsh.model.mesh.getAllEdges()
+        print(len(self.edgeTags), " ", len(self.edgeNodes))
+        # print(self.edgeTags)
+        # 获取网格中面的个数
+        gmsh.model.mesh.createFaces()
+        # faceNodes是faceTags的三倍，每个面有三个节点
+        self.faceTags, self.faceNodes = gmsh.model.mesh.getAllFaces(3)
+        print(gmsh.model.mesh.getElements(2))
+        print(len(self.faceTags), " ", len(self.faceNodes))
+        # print(self.faceTags)
         gmsh.clear()
         gmsh.finalize()
-    pass
+        pass
+
+    def Trans_Geometry(file_paths, output_path, output_format):
+        for file_path in file_paths:
+            out_last_name = output_format.split("*")[1][:-1]
+            # print(out_last_name)
+            # print(file_path)
+            # print(os.path.split(file_path))
+            # print(output_format)
+            gmsh.initialize()
+            gmsh.model.add("model")
+            gmsh.open(file_path)
+            out_name = os.path.splitext(os.path.split(file_path)[1])
+            print(os.path.join(output_path, out_name[0]) + out_last_name)
+            # pause=input("Press Enter to continue...")
+            gmsh.write(os.path.join(output_path, out_name[0]) + out_last_name)
+            gmsh.clear()
+            gmsh.finalize()
+
+    def Trans_Mesh(file_paths, output_path, output_format):
+        for file_path in file_paths:
+            out_last_name = output_format.split("*")[1][:-1]
+            # print(out_last_name)
+            # print(file_path)
+            # print(os.path.split(file_path))
+            # print(output_format)
+            gmsh.initialize()
+            gmsh.model.add("model")
+            gmsh.open(file_path)
+            out_name = os.path.splitext(os.path.split(file_path)[1])
+            print(os.path.join(output_path, out_name[0]) + out_last_name)
+            # pause=input("Press Enter to continue...")
+            gmsh.write(os.path.join(output_path, out_name[0]) + out_last_name)
+            gmsh.clear()
+            gmsh.finalize()
+        pass
+
+
+# 以下用于测试
+# if __name__ == "__main__":
+#     MyMesh = MyTranslator()
+#     MyMesh.Get_Mesh_Dim_Elements("files/output.mesh")
--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog, QTextEdit, QListWidget, QListWidgetItem, QHBoxLayout, QWidget, QMenuBar
-from PyQt5.QtWidgets import QComboBox, QLabel, QVBoxLayout, QPushButton, QSizePolicy, QStatusBar, QMessageBox
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QAction,
+    QFileDialog,
+    QTextEdit,
+    QListWidget,
+    QListWidgetItem,
+    QHBoxLayout,
+    QWidget,
+    QMenuBar,
+)
+from PyQt5.QtWidgets import (
+    QComboBox,
+    QLabel,
+    QVBoxLayout,
+    QPushButton,
+    QSizePolicy,
+    QStatusBar,
+    QMessageBox,
+)
 import Translator
 import sys
+
 
 class MyClass(object):
     def __init__(self):
@@ -19,7 +39,7 @@ class MyClass(object):
 
 
 class MyCentralWidget(QWidget):
-    
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setLayout(QHBoxLayout())
@@ -31,7 +51,7 @@ class MyCentralWidget(QWidget):
         self.list_widget.itemClicked.connect(self.item_clicked)
         self.layout().setStretchFactor(self.list_widget, 1)
         self.layout().setStretchFactor(self.text_edit, 3)
-        #把下面的控件垂直排列
+        # 把下面的控件垂直排列
         vbox1 = QVBoxLayout()
         self.layout().addLayout(vbox1)
         self.output_button = QPushButton("导出")
@@ -41,23 +61,25 @@ class MyCentralWidget(QWidget):
         self.combobox_output_format.setSizeAdjustPolicy(QComboBox.AdjustToContents)
         self.combobox_output_format.currentTextChanged.connect(self.ChooseOutputFormat)
         vbox1.addWidget(self.label_output_format)
-        #加入一个combobox
+        # 加入一个combobox
         vbox1.addWidget(self.combobox_output_format)
         vbox1.addWidget(self.output_button)
-        #其他条件设置良好之前，导出按钮不可用
+        # 其他条件设置良好之前，导出按钮不可用
         self.output_button.setEnabled(False)
         self.output_button.clicked.connect(self.ExportFile)
         vbox1.addStretch(2)
-        
+
     def ChooseOutputFormat(self):
-        self.parent().status.showMessage("Output Format: "+self.combobox_output_format.currentText())
+        self.parent().status.showMessage(
+            "Output Format: " + self.combobox_output_format.currentText()
+        )
 
     def load_data(self, file_paths):  # 参数名改为 file_paths 以反映它是一个列表
-        #清空文本框
+        # 清空文本框
         self.text_edit.clear()
-        #清空列表
+        # 清空列表
         self.list_widget.clear()
-        if(not file_paths):
+        if not file_paths:
             return
         self.input_file_paths = file_paths
         for file_path in file_paths:  # 遍历列表中的每个文件路径
@@ -66,9 +88,10 @@ class MyCentralWidget(QWidget):
             # 为每个文件创建一个 QListWidgetItem 并添加到列表中
             item = QListWidgetItem(file_path)
             self.list_widget.addItem(item)
+
     def item_clicked(self, item):
         # 状态栏输出文件名
-        self.parent().status.showMessage("Choose: "+item.text())
+        self.parent().status.showMessage("Choose: " + item.text())
         with open(item.text(), "r") as f:
             file_data = f.readlines()
         self.text_edit.clear()
@@ -77,29 +100,31 @@ class MyCentralWidget(QWidget):
         #     self.text_edit.append(line)
 
     def ExportFile(self):
-        file_paths=self.input_file_paths
+        file_paths = self.input_file_paths
         print(file_paths)
-        output_format=self.combobox_output_format.currentText()
-        output_path=self.parent().folder_path
-        if(self.parent().InputType=="Geometry"):
-            Translator.Trans_Geometry(file_paths, output_path, output_format)
-        elif(self.parent().InputType=="Mesh"):
-            Translator.Trans_Mesh(file_paths, output_path, output_format)
+        output_format = self.combobox_output_format.currentText()
+        output_path = self.parent().folder_path
+        if self.parent().InputType == "Geometry":
+            Translator.MyTranslator.Trans_Geometry(
+                file_paths, output_path, output_format
+            )
+        elif self.parent().InputType == "Mesh":
+            Translator.MyTranslator.Trans_Mesh(file_paths, output_path, output_format)
 
     def Show_Button(self):
         print("Show_Button")
         try:
-            if(self.input_file_paths and self.parent().folder_path):
+            if self.input_file_paths and self.parent().folder_path:
                 self.output_button.setEnabled(True)
         except:
             self.output_button.setEnabled(False)
+
 
 class MyMainWindow(QMainWindow):
     def __init__(self):
         super(MyMainWindow, self).__init__()
         self.initMenu()
         self.initUI()
-        
 
     def initUI(self):
         self.setWindowTitle("格式转换器")
@@ -112,7 +137,7 @@ class MyMainWindow(QMainWindow):
         self.status = self.statusBar()
         self.central = MyCentralWidget(self)
 
-        self.status.showMessage('Ready')
+        self.status.showMessage("Ready")
 
         self.file_menu = self.menu.addMenu("文件")
         self.file_menu_open = self.file_menu.addMenu("打开")
@@ -134,66 +159,72 @@ class MyMainWindow(QMainWindow):
         self.exit_action = QAction("退出", self)
         self.exit_action.triggered.connect(self.close)
         self.file_menu.addAction(self.exit_action)
-        
-        self.introduction=QAction("关于",self)
+
+        self.introduction = QAction("关于", self)
         self.menu.addAction(self.introduction)
         self.introduction.triggered.connect(self.Show_Introduction)
 
     def open_file_geometry(self):
         file_path, _ = QFileDialog.getOpenFileNames(
-            self, "Open File", "", 
-            "All Files (*.*);;" +
-            "Geometry - Gmsh GEO (*.geo);;" + 
-            "Geometry - OpenCASCADE BREP (*.brep);;" + 
-            "Geometry - OpenCASCADE XAO (*.xao);;" + 
-            "Geometry - STEP (*.stp *.step);;" + 
-            "Geometry - IGES (*.igs *.iges)"
+            self,
+            "Open File",
+            "",
+            "All Files (*.*);;"
+            + "Geometry - Gmsh GEO (*.geo);;"
+            + "Geometry - OpenCASCADE BREP (*.brep);;"
+            + "Geometry - OpenCASCADE XAO (*.xao);;"
+            + "Geometry - STEP (*.stp *.step);;"
+            + "Geometry - IGES (*.igs *.iges)",
         )
         if file_path:
             self.central.load_data(file_path)
-            self.status.showMessage('Geometry file loaded')
-            self.InputType="Geometry"
+            self.status.showMessage("Geometry file loaded")
+            self.InputType = "Geometry"
             self.ChangeComboBox()
 
     def open_file_mesh(self):
         file_path, _ = QFileDialog.getOpenFileNames(
-            self, "Open File", "", 
-            "All Files (*.*);;" +
-            "Mesh - Gmsh MSH (*.msh);;" +
-            "Mesh - Diffpack 3D (*.diff);;" +
-            "Mesh - I-deas Universal (*.unv);;" +
-            "Mesh - MED (*.med *.mmed);;" +
-            "Mesh - Inria Medit (*.mesh);;" +
-            "Mesh - Nastran Bulk Data File (*.bdf *.nas);;" +
-            "Mesh - Gambit Neutral (*.neu);;" +
-            "Mesh - Object File Format (*.off);;" +
-            "Mesh - Plot3D Structured Mesh (*.p3d);;" +
-            "Mesh - STL Surface(*.stl);;" +
-            "Mesh - VTK (*.vtk);;" +
-            "Mesh - VRML Surface (*.wrl *vrml);;" +
-            "Mesh - PLY2 (*.ply)"
+            self,
+            "Open File",
+            "",
+            "All Files (*.*);;"
+            + "Mesh - Gmsh MSH (*.msh);;"
+            + "Mesh - Diffpack 3D (*.diff);;"
+            + "Mesh - I-deas Universal (*.unv);;"
+            + "Mesh - MED (*.med *.mmed);;"
+            + "Mesh - Inria Medit (*.mesh);;"
+            + "Mesh - Nastran Bulk Data File (*.bdf *.nas);;"
+            + "Mesh - Gambit Neutral (*.neu);;"
+            + "Mesh - Object File Format (*.off);;"
+            + "Mesh - Plot3D Structured Mesh (*.p3d);;"
+            + "Mesh - STL Surface(*.stl);;"
+            + "Mesh - VTK (*.vtk);;"
+            + "Mesh - VRML Surface (*.wrl *vrml);;"
+            + "Mesh - PLY2 (*.ply)",
         )
         if file_path:
             self.central.load_data(file_path)
-            self.status.showMessage('Mesh file loaded')
-            self.InputType="Mesh"
+            self.status.showMessage("Mesh file loaded")
+            self.InputType = "Mesh"
             self.ChangeComboBox()
 
     def export_file(self):
-        self.folder_path = QFileDialog.getExistingDirectory(self, '选择文件夹', '/')
+        self.folder_path = QFileDialog.getExistingDirectory(self, "选择文件夹", "/")
         if self.folder_path:
-            self.status.showMessage('Export to: ' + self.folder_path)
+            self.status.showMessage("Export to: " + self.folder_path)
 
     def ChangeComboBox(self):
         self.central.combobox_output_format.clear()
-        if(self.InputType=="Geometry"):
+        if self.InputType == "Geometry":
             self.central.combobox_output_format.addItem("Gmsh Options (*.opt)")
-            self.central.combobox_output_format.addItem("Gmsh Unrolled GEO (*.geo_unrolled)")
+            self.central.combobox_output_format.addItem(
+                "Gmsh Unrolled GEO (*.geo_unrolled)"
+            )
             self.central.combobox_output_format.addItem("OpenCASCADE BREP (*.brep)")
             self.central.combobox_output_format.addItem("OpenCASCADE XAO (*.xao)")
             self.central.combobox_output_format.addItem("STEP (*.step)")
             self.central.combobox_output_format.addItem("IGES (*.iges)")
-        elif(self.InputType=="Mesh"):
+        elif self.InputType == "Mesh":
             self.central.combobox_output_format.addItem("Gmsh MSH (*.msh)")
             self.central.combobox_output_format.addItem("Abaqus INP (*.inp)")
             self.central.combobox_output_format.addItem("LSDYNA KEY (*.key)")
@@ -207,9 +238,13 @@ class MyMainWindow(QMainWindow):
             self.central.combobox_output_format.addItem("Inria Medit (*.mesh)")
             self.central.combobox_output_format.addItem("CEA Triangulation (*.mail)")
             self.central.combobox_output_format.addItem("Matlab (*.m)")
-            self.central.combobox_output_format.addItem("Nastran Bulk Data File (*.bdf)")
+            self.central.combobox_output_format.addItem(
+                "Nastran Bulk Data File (*.bdf)"
+            )
             self.central.combobox_output_format.addItem("Object File Format (*.off)")
-            self.central.combobox_output_format.addItem("Plot3D Structured Mesh (*.p3d)")
+            self.central.combobox_output_format.addItem(
+                "Plot3D Structured Mesh (*.p3d)"
+            )
             self.central.combobox_output_format.addItem("STL Surface(*.stl)")
             self.central.combobox_output_format.addItem("VTK (*.vtk)")
             self.central.combobox_output_format.addItem("VRML Surface (*.wrl)")
@@ -219,9 +254,14 @@ class MyMainWindow(QMainWindow):
             self.central.combobox_output_format.addItem("GAMBIT Neutral File (*.neu)")
             self.central.combobox_output_format.addItem("X3D (*.x3d)")
             pass
-    
+
     def Show_Introduction(self):
-        QMessageBox.about(self,"关于","这是一个格式转换器，可以将几何文件和网格文件转换为不同的格式。请先选择文件，再选择输出地址，再选择输出格式，然后可以导出。")
+        QMessageBox.about(
+            self,
+            "关于",
+            "这是一个格式转换器，可以将几何文件和网格文件转换为不同的格式。请先选择文件，再选择输出地址，再选择输出格式，然后可以导出。",
+        )
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
